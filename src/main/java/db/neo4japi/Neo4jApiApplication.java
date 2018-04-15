07207eef6f4910686cb41f27fa38cb870fd425ab
romanfomin <rofominucvt@mail.ru>
2018-04-15 22:47:02 +0300

    1

---
+++ src/main/java/db/neo4japi/Neo4jApiApplication.java
@@ -0,0 +1,12 @@
+package db.neo4japi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jApiApplication.class, args);
+    }
+}
